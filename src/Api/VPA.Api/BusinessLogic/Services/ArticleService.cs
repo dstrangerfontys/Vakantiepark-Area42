cc2b42968e8acd6640c1eb732c2bc7aa3bbf771f
Jirho <92875784+Jirhonet@users.noreply.github.com>
2025-05-15 23:33:29 +0200

    Added example of service

---
+++ src/Api/VPA.Api/BusinessLogic/Services/ArticleService.cs
@@ -0,0 +1,22 @@
+﻿using VPA.Api.Repositories;
+using VPA.Models;
+
+namespace VPA.Api.Services
+{
+    public class ArticleService
+    {
+        private readonly ArticleRepository repository;
+
+        public ArticleService(ArticleRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Task<IEnumerable<Article>> GetAsync(CancellationToken ct = default)
+        {
+            // Hier kunnen we eventueel logica toevoegen
+
+            return repository.GetAsync(ct);
+        }
+    }
+}
--- src/Api/VPA.Api/Controllers/ArticlesController.cs
+++ src/Api/VPA.Api/Controllers/ArticlesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using VPA.Api.Repositories;
+using VPA.Api.Services;
 using VPA.Models;
 
 namespace VPA.Api.Controllers
@@ -7,17 +8,19 @@ namespace VPA.Api.Controllers
     public class ArticlesController : ApiController
     {
         private readonly ArticleRepository repository;
+        private readonly ArticleService service;
 
-        public ArticlesController(ArticleRepository repository)
+        public ArticlesController(ArticleRepository repository, ArticleService service)
         {
             this.repository = repository;
+            this.service = service;
         }
 
         [HttpGet]
         [Route("")]
         public Task<IEnumerable<Article>> Get(CancellationToken ct = default)
         {
-            return repository.GetAsync(ct);
+            return service.GetAsync(ct);
         }
 
         [HttpGet]
