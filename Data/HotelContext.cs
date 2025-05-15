67187df12db8a8907ef39ccc54eba3fe64f5f647
Jirho <92875784+Jirhonet@users.noreply.github.com>
2025-05-15 19:31:53 +0200

    format

--- Data/HotelContext.cs
+++ Data/HotelContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Vakantiepark_Area42.Models.Entities;
 
-
 namespace Vakantiepark_Area42.Data
 {
     public class HotelContext : DbContext
     {
-        public HotelContext(DbContextOptions<HotelContext> options) : base(options) { }
+        public HotelContext(DbContextOptions<HotelContext> options)
+            : base(options)
+        {
+            //
+        }
 
         public DbSet<Guest> Guest { get; set; }
         public DbSet<Room> Room { get; set; }
