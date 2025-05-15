35ecb688e3426a75c1a204a4843baca73d6da2f0
Jirho <92875784+Jirhonet@users.noreply.github.com>
2025-05-15 19:28:57 +0200

    Updated Program Created extension method for adding dbcontext

--- Program.cs
+++ Program.cs
@@ -1,33 +1,43 @@
-using Vakantiepark_Area42.Data; 
 using Microsoft.EntityFrameworkCore;
+using Vakantiepark_Area42.Data;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAuthorization();
-builder.Services.AddRazorPages();
-
-builder.Services.AddDbContext<HotelContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Vakantiepark_Area42
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddContext<HotelContext>(builder.Configuration);
+
+            WebApplication app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+
+        private static IServiceCollection AddContext<T>(this IServiceCollection services, ConfigurationManager configuration)
+            where T : DbContext
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            ServerVersion serverVersion = ServerVersion.AutoDetect(connectionString);
+
+            return services.AddDbContext<T>(options => options.UseMySql(connectionString, serverVersion));
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
